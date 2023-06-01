3b39091d03855ea983ffef9fb52669bd5a53049d
IkramBalti <129930621+IkramBalti@users.noreply.github.com>
2023-06-01 10:59:12 +0500

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        //console.write("Hello Pakistan");
+        //cconsole.WriteLine("Hello Pakistan");
+        //console.ReadLine();
+
+        //console.WriteLine(6+5);    //11
+        //console.WriteLine(8-4);    //4
+        //console.WriteLine(6.0/5);    //1.2
+        //console.WriteLine(5*6);    //30
+        //console.WriteLine(5%6);
+        //console.WriteLine(5 % 6);
+        //console.WriteLine(5++);
+        //console.WriteLine(5--);
+
+        // > Greater than
+        // < Less than
+        // >= Greater than or equal to 
+        // <= Less than or equal to
+        // != Equal or not
+
+        //console.WriteLine(20>10); //true
+        //console.WriteLine(10<20);  //true
+        //console.WriteLine(20>=10); //true
+        //console.WriteLine(10<=20); //true
+        //console.WriteLine(20!=10); //true
+
+        //console.WriteLine(20 | 10);
+        //console.WriteLine(20 & 10);
+        //console.WriteLine(20 | 10);
+        //console.WriteLine(20 & 10);
+
+        Console.WriteLine("Hello \n Pakistan");
+        Console.WriteLine("");
+
+
+
+        console.ReadLine();
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,138 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("I love Pakistan");
+//Console.Write("Hello");
+//Console.WriteLine("I am from Gilgit Baltistan");
+
+//Console.WriteLine(6+5);    //11
+//Console.WriteLine(8-4);    //4
+//Console.WriteLine(6.0/5);    //1.2
+//Console.WriteLine(5*6);    //30
+//Console.WriteLine(5%6);    //5
+//Console.WriteLine(5 % 6);   //5
+//Console.WriteLine(5++);
+//Console.WriteLine(5--);
+
+
+// > Greater than
+// < Less than
+// >= Greater than or equal to 
+// <= Less than or equal to
+// != Equal or not
+
+//Console.WriteLine(20>10); //true
+//Console.WriteLine(10<20);  //true
+//Console.WriteLine(20>=10); //true
+//Console.WriteLine(10<=20); //true
+//Console.WriteLine(20!=10); //true
+
+
+// Console.WriteLine(20 | 10);  //30
+// Console.WriteLine(20 & 10);  //0
+// Console.WriteLine(97 | 49);  //113
+// Console.WriteLine(97 & 49);  //33
+
+
+// Console.WriteLine("Hello \n Pakistan"); //line break
+// Console.WriteLine("Hello \t Pakistan"); //give space between
+// Console.WriteLine("Hello \'a\b Pakistan");
+
+
+// string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+// Console.WriteLine("The length of the txt string is: " + txt.Length); //length of text
+
+
+//string & float price
+//int age =20;  //declaration + initialization
+//int age;//declaration
+//int ages =22;  // initialization
+
+//Console.WriteLine(age); //20
+//Console.WriteLine(ages); //22
+
+//float price = 500.99f;
+//float frq = 500.99999999f;
+//float fraq = 500.55555f;
+//decimal ab = 50.000000;
+//Console.WriteLine(price);
+//Console.WriteLine(frq);
+//Console.WriteLine(fraq);
+//Console.WriteLine(ab);
+
+//char key =" ";
+//Console.WriteLine(key);
+
+//string name =”IkramUllah”;
+//Console.WriteLine(name);
+
+
+//string firstName = "Ikram";
+//string lastName = "Ullah";
+//string name = firstName + lastName;  //IkramUllah
+
+//string firstName = "Ikram";
+//string lastName = "Ullah";
+//Console.WriteLine(firstName + "  " + lastName); //Ikram Ullah
+
+
+//bool isCSharp = true;
+//bool isFishTasty = false;
+//Console.WriteLine(isCSharp);   // Outputs True
+//Console.WriteLine(isFishTasty);   // Outputs False
+
+
+// int sugerPrice = 100, ricePrice = 130, breadPrice = 160;
+// sugerPrice=125;
+// Console.WriteLine("Suger Price Is = " sugerPrice "Rice Price is = " + ricePrice "Bread Price is = " + breadPrice);
+
+//string firstName = "Ikram";
+//string lastName = "Ullah";
+//string name = $"My full name is: {firstName} {lastName}";
+//Console.WriteLine(name);
+
+//Const schoolName= "kips School";
+//Console.WriteLine(schoolName);
+
+//string txt = "I am \"IkramUllah\" from the north Pakistan.";
+// Console.WriteLine(txt);
+
+//string tx = "I am \'IkramUllah\' from the north Pakistan.";
+//Console.WriteLine(tx);
+
+//string text = "I am \\IkramUllah\\ from the north Pakistan.";
+// Console.WriteLine(text);
+
+
+//int x = 10;
+//int y = 9;
+//Console.WriteLine(x > y); // True, because 10 is greater than 9
+
+/*int x = 10;
+int y = 9;
+Console.WriteLine(x + y); //19
+
+
+int myAge = 25;
+int votingAge = 18;
+Console.WriteLine(myAge >= votingAge); //true
+*/
+
+/* if (myAge >= votingAge)
+{
+    Console.WriteLine("Old enough to vote!");
+}
+else
+{
+    Console.WriteLine("Not old enough to vote.");
+}
+*/
+
+int x = 50;
+int y = 10;
+if (x > y)
+{
+    Console.WriteLine("Hello World");
+}
+
+
+Console.ReadLine();
